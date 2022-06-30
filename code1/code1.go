d5c8e2058dd104a21e6faf399a4cd63a5269821f
linliyuan <linliyuan@37.com>
2022-06-30 14:41:53 +0800

    commit

---
+++ code1/code1.go
@@ -0,0 +1,20 @@
+package code1
+
+import "fmt"
+
+func Code1Fmt(str string) {
+	if str == "1" {
+		fmt.Println("Code1Fmt1111")
+		return
+	}
+	fmt.Println("Code1Fmt2222")
+	return
+}
+
+func Code1Hello(str string) {
+	if str == "1" {
+		fmt.Println("hello word")
+		return
+	}
+	fmt.Println("Code1Fmt2222")
+}
