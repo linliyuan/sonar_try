0d0ebd8eb8e7beac61acfa2d604d49c94af2e83e
linliyuan <linliyuan@37.com>
2022-07-01 15:50:15 +0800

    sda

---
+++ code2/util/array.go
@@ -0,0 +1,42 @@
+package util
+
+import (
+	"reflect"
+)
+
+func InArrayStr(needle string, haystack []string) bool {
+	for i := 0; i < len(haystack); i++ {
+		if haystack[i] == needle {
+			return true
+		}
+	}
+	return false
+}
+
+func InArrayInt(needle int, haystack []int) bool {
+	for i := 0; i < len(haystack); i++ {
+		if haystack[i] == needle {
+			return true
+		}
+	}
+	return false
+}
+
+func InArray(needle interface{}, haystack interface{}) bool {
+	val := reflect.ValueOf(haystack)
+	switch val.Kind() {
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < val.Len(); i++ {
+			if reflect.DeepEqual(needle, val.Index(i).Interface()) {
+				return true
+			}
+		}
+	case reflect.Map:
+		for _, k := range val.MapKeys() {
+			if reflect.DeepEqual(needle, val.MapIndex(k).Interface()) {
+				return true
+			}
+		}
+	}
+	return false
+}
